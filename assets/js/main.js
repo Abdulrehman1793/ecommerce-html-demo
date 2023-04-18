4f77f3319275a0e4ff0f6e54dbfec11c060a6e56
KHAN ABDULREHMAN <abdulrehman1793@gmail.com>
2023-04-18 21:30:37 +0530

    ✔ screen upto 1200

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,10 +1,23 @@
 /*=============== SHOW MENU ===============*/
+var navMenu = document.getElementById("nav-menu"),
+  navToggle = document.getElementById("nav-toggle"),
+  navClose = document.getElementById("nav-close");
 
 /*===== Menu Show =====*/
 /* Validate if constant exists */
+if (navToggle) {
+  navToggle.addEventListener("click", () => {
+    navMenu.classList.add("show-menu");
+  });
+}
 
 /*===== Hide Show =====*/
 /* Validate if constant exists */
+if (navClose) {
+  navClose.addEventListener("click", () => {
+    navMenu.classList.remove("show-menu");
+  });
+}
 
 /*=============== IMAGE GALLERY ===============*/
 function imgGallery() {
@@ -24,18 +37,27 @@ imgGallery();
 var swiperCategories = new Swiper(".categories__container", {
   spaceBetween: 24,
   loop: true,
+
   navigation: {
     nextEl: ".swiper-button-next",
     prevEl: ".swiper-button-prev",
   },
   breakpoints: {
-    640: {
+    350: {
       slidesPerView: 2,
-      spaceBetween: 20,
+      spaceBetween: 24,
     },
     768: {
+      slidesPerView: 3,
+      spaceBetween: 24,
+    },
+    992: {
       slidesPerView: 4,
-      spaceBetween: 40,
+      spaceBetween: 24,
+    },
+    1200: {
+      slidesPerView: 5,
+      spaceBetween: 24,
     },
     1400: {
       slidesPerView: 6,
@@ -48,18 +70,19 @@ var swiperCategories = new Swiper(".categories__container", {
 var swiperProducts = new Swiper(".new__container", {
   spaceBetween: 24,
   loop: true,
+
   navigation: {
     nextEl: ".swiper-button-next",
     prevEl: ".swiper-button-prev",
   },
   breakpoints: {
-    640: {
-      slidesPerView: 2,
-      spaceBetween: 20,
-    },
     768: {
-      slidesPerView: 4,
-      spaceBetween: 40,
+      slidesPerView: 2,
+      spaceBetween: 24,
+    },
+    992: {
+      slidesPerView: 3,
+      spaceBetween: 24,
     },
     1400: {
       slidesPerView: 4,
